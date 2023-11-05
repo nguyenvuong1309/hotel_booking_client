5a83f2780eba720c6d17dc959cd205f987c8f8b2
nguyenvuong1309 <21522809@gm.uit.edu.vn>
2023-11-05 20:16:52 +0700

    commit

---
+++ src/UserContext.jsx
@@ -0,0 +1,26 @@
+// const { createContext } = require("react");
+
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+
+
+export const UserContext = createContext({});
+export function UserContextProvider({ children }) {
+    const [count, setCount] = useState(0)
+    const [ready, setReady] = useState(false);
+    useEffect(() => {
+        if (!user) {
+            axios.get('/profile').then(({ data }) => {
+                setUser(data)
+                setReady(true)
+            })
+        }
+    }, [])
+    const [user, setUser] = useState(null);
+    return (
+        <UserContext.Provider value={{ user, setUser, ready, setReady }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ProfilePage() {
+    return (
+        <>
+            <h1>Profile Page</h1>
+        </>
+    )
+}
