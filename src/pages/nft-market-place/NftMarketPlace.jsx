5edf31f9aaaa9806f1ef8143f9e16bfbbd24422b
nguyenvuong1309 <21522809@gm.uit.edu.vn>
2023-11-22 17:20:30 +0700

    sua phan nft market place

--- src/pages/nft-market-place/NftMarketPlace.jsx
+++ src/pages/nft-market-place/NftMarketPlace.jsx
@@ -18,13 +18,21 @@ export default function NftMarketPlace() {
 
     const { data: nfts, isLoading, error } = useNFTs(contract);
     console.log("🚀 ~ file: NftMarketPlace.jsx:20 ~ NftMarketPlace ~ nfts:", nfts)
+
+    console.log("🚀 ~ file: NftMarketPlace.jsx:20 ~ NftMarketPlace ~ nfts:", nfts)
     const { data: contractName } = useContractRead(contract, 'name')
 
 
     const handleBurnNFT = async (id) => {
         const result = await contract.erc721.burn(id);
     }
-
+    const CheckNumberOfNFT = () => {
+        let count = 0;
+        nfts?.map((nft) => (
+            nft?.owner == ownerAddress ? count = count + 1 : count
+        ))
+        return count
+    }
     return (
         <div className="mb-96">
             <div className="mt-5">
@@ -37,28 +45,36 @@ export default function NftMarketPlace() {
             </div>
             {
                 <div className="flex justify-center">
-                    <div className="grid gap-10 grid-cols-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10">
-                        {
-                            isLoading ? (
-                                <div>Loading...</div>
+                    {
+                        ownerAddress ? (
+                            CheckNumberOfNFT() === 0 ? (
+                                <div>
+                                    <div className="flex items-center justify-center font-bold mb-10">
+                                        Don't have any NFT
+                                    </div>
+                                    <Link to={"/nft-market-place/buy"} className=" bg-slate-400 flex rounded-sm items-center justify-center">
+                                        Go to market place
+                                    </Link>
+                                </div>
                             ) : (
-                                nfts.map((nft) => (
-                                    nft?.owner == ownerAddress && (
-
-                                        //  <Link to={`/nft-market-place/${nft?.metadata?.id}`} state={{ id: nft?.metadata?.id }} className="grid bg-slate-300 rounded justify-center" key={nft?.metadata?.id} >
-                                        <NFTItem nft={nft} key={nft?.id} />
-                                        //     <div className="flex justify-around items-center">
-                                        //         <div className="flex h-20 justify-center items-center font-bold">
-                                        //             {nft.metadata.name}
-                                        //         </div>
-                                        //         <ButtonBurnAnNFT id={nft?.metadata?.id} />
-                                        //     </div>
-                                        // </Link>
-                                    )
-                                ))
+                                <div className="grid gap-10 grid-cols-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10">
+                                    {isLoading ? (
+                                        <div>Loading...</div>
+                                    ) : (
+                                        nfts.map((nft) => (
+                                            nft?.owner == ownerAddress && (
+                                                <NFTItem nft={nft} key={nft?.id} />
+                                            )
+                                        ))
+                                    )}
+                                </div>
                             )
-                        }
-                    </div>
+                        ) : (
+                            <div className="flex items-center justify-center font-bold">
+                                Login to view and buy NFT
+                            </div>
+                        )
+                    }
                 </div>
             }
         </div>
